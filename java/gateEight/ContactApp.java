561ff263f417a61c57e13e04f0ab238bebfa0078
Agatha00133 <agathansofor1@gmail.com>
2026-03-26 14:13:38 +0100

    contactapp

--- java/gateEight/ContactApp.java
+++ java/gateEight/ContactApp.java
@@ -1,37 +1,69 @@
 import java.util.Scanner;
-import.java.util.Arraylist;
+import java.util.ArrayList;
     public class ContactApp{
+    
+    static ArrayList<String> contacts = new ArrayList<>();
+    static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args){
-public static void ContactApp menu(){
-    Scanner input = new Scanner(System.in);
-    System.out.print("=====welcome To ContactApp=====");    
-    System.out.println("1.Add contact needs : ");
+
+    System.out.print("=====welcome To ContactApp===== ");    
+    System.out.println("\n1.Add contact needs : ");
     System.out.println("2. Remove contact : ");
     System.out.println("3. Find contact by phone : ");
     System.out.println("4. find contact by first name : ");
     System.out.println("5. find contact by last name : ");
     System.out.println("6. Edit contact : ");
 
+     System.out.print("Choose option: ");
+     int choice = input.nextInt();
+     input.nextLine();
 
-    String firstName;
-    String lastName;
-    String phoneNumber;
+        if (choice == 1) {
+            ContactAppAddcontactNeeds();
+        } else if (choice == 2) {
+            ContactAppRemoveContact();
+        } else {
+            System.out.println("Invalid option!");
+        }
+    }
 
-public static void ContactApp Add contact needs (){
+    
+    public static void ContactAppAddcontactNeeds(){
     System.out.print("Enter First Name : ");
-    firstName = input.nextLine();
+    String firstName = input.nextLine();
 
     System.out.print("Enter Last Name : ");
-    lastName = input.nextLine();
+    String lastName = input.nextLine();
 
     System.out.print("Enter phone Number : ");
-    phoneNumber = input.nextLine();
-    System.out.print("contact added successfully!");
+    String phoneNumber = input.nextLine();
+
+    String contact = firstName + " " + lastName + " - " + phoneNumber;
+    contacts.add(contact);
+
+    System.out.println("contact added successfully!");
+}
  
-public static void ContactApp remove contact(){
-   
-}
-}
-}
+    public static void ContactAppRemoveContact(){
+    System.out.print("Enter PhoneNumber to delete: ");
+    String phoneNumber = input.nextLine();
+    
+            boolean found = false;
+
+        for (int i = 0; i < contacts.size(); i++) {
+            if (contacts.get(i).contains(phoneNumber)) {
+                contacts.remove(i);
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("contact removed successfully!");
+            }else{
+System.out.print("cont not found ! ");
+    }
     }
 }
+
