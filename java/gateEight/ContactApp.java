153c4a40e8720786f2f00c6b040351f66c11b194
Agatha00133 <agathansofor1@gmail.com>
2026-03-26 12:29:35 +0100

    recent work

---
+++ java/gateEight/ContactApp.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import.java.util.Arraylist;
+    public class ContactApp{
+    public static void main(String[] args){
+public static void ContactApp menu(){
+    Scanner input = new Scanner(System.in);
+    System.out.print("=====welcome To ContactApp=====");    
+    System.out.println("1.Add contact needs : ");
+    System.out.println("2. Remove contact : ");
+    System.out.println("3. Find contact by phone : ");
+    System.out.println("4. find contact by first name : ");
+    System.out.println("5. find contact by last name : ");
+    System.out.println("6. Edit contact : ");
+
+
+    String firstName;
+    String lastName;
+    String phoneNumber;
+
+public static void ContactApp Add contact needs (){
+    System.out.print("Enter First Name : ");
+    firstName = input.nextLine();
+
+    System.out.print("Enter Last Name : ");
+    lastName = input.nextLine();
+
+    System.out.print("Enter phone Number : ");
+    phoneNumber = input.nextLine();
+    System.out.print("contact added successfully!");
+ 
+public static void ContactApp remove contact(){
+   
+}
+}
+}
+    }
+}
